e03d1e85ebd74c2af150cbee83195be3a6ba404d
Samuil Berenzon <s.berenzon@mediaevent.services>
2026-07-16 18:13:39 +0200

    replace images

--- src/data/kandidaten.ts
+++ src/data/kandidaten.ts
@@ -15,9 +15,9 @@ export interface Kandidat {
  * and portraits downloaded locally to /public/pics/kandis/<slug>.
  */
 export const KANDIDATEN: Kandidat[] = [
-  { name: 'Anna Auerbach', slug: 'anna-auerbach', listenplatz: 1, bezirk: 'Mitte', image: '/pics/kandis/anna-auerbach.jpg' },
+  { name: 'Anna Auerbach', slug: 'anna-auerbach', listenplatz: 1, bezirk: 'Mitte', image: '/pics/kandis/anna-auerbach.png' },
   { name: 'Paul Loeper', slug: 'paul-loeper', listenplatz: 2, bezirk: 'Pankow', image: '/pics/kandis/paul-loeper.png' },
-  { name: 'Rafael Kaaz', slug: 'rafael-kaaz', listenplatz: 3, bezirk: 'Charlottenburg-Wilmersdorf', image: '/pics/kandis/rafael-kaaz.jpg' },
+  { name: 'Rafael Kaaz', slug: 'rafael-kaaz', listenplatz: 3, bezirk: 'Charlottenburg-Wilmersdorf', image: '/pics/kandis/rafael-kaaz.png' },
   { name: 'Pia Voltz', slug: 'pia-voltz', listenplatz: 4, bezirk: 'Treptow-Köpenick', image: '/pics/kandis/pia-voltz.png' },
   { name: 'Jakob Johannes Welker', slug: 'jakob-johannes-welker', listenplatz: 5, bezirk: 'Friedrichshain-Kreuzberg', image: '/pics/kandis/jakob-johannes-welker.jpg' },
   { name: 'Aiga Marie Senftleben', slug: 'aiga-marie-senftleben', listenplatz: 6, bezirk: 'Pankow', image: '/pics/kandis/aiga-marie-senftleben.png' },
@@ -28,7 +28,7 @@ export const KANDIDATEN: Kandidat[] = [
   { name: 'Christian Weißsteiner', slug: 'christian-weisssteiner', listenplatz: 11, bezirk: 'Charlottenburg-Wilmersdorf', image: '/pics/kandis/christian-weisssteiner.png' },
   { name: 'Susanne Maria Zels', slug: 'susanne-maria-zels', listenplatz: 12, bezirk: 'Friedrichshain-Kreuzberg', image: '/pics/kandis/susanne-maria-zels.jpg' },
   { name: 'Martin Hergert', slug: 'martin-hergert', listenplatz: 13, bezirk: 'Mitte', image: '/pics/kandis/martin-hergert.jpg' },
-  { name: 'Rahel Demant', slug: 'rahel-demant', listenplatz: 14, bezirk: 'Tempelhof-Schöneberg', image: '/pics/kandis/rahel-demant.jpg' },
+  { name: 'Rahel Demant', slug: 'rahel-demant', listenplatz: 14, bezirk: 'Tempelhof-Schöneberg', image: '/pics/kandis/rahel-demant.png' },
   { name: 'Alexander Paulski', slug: 'alexander-paulski', listenplatz: 15, bezirk: 'Neukölln', image: '/pics/kandis/alexander-paulski.jpg' },
   { name: 'Juliane Kalbacher', slug: 'juliane-kalbacher', listenplatz: 16, bezirk: 'Charlottenburg-Wilmersdorf', image: '/pics/kandis/juliane-kalbacher.png' },
   { name: 'Ingo Partey', slug: 'ingo-partey', listenplatz: 17, bezirk: 'Steglitz-Zehlendorf', image: '/pics/kandis/ingo-partey.png' },
